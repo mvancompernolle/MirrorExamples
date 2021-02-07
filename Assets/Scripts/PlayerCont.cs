81dbb2c572b9d25a0487bd57d6af339e8931898d
mvancompernolle <mvancomp@gmail.com>
2021-02-07 10:31:31 -0800

    basic mult movement example

---
+++ Assets/Scripts/PlayerCont.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerCont : NetworkBehaviour
+{
+    public float movementSpeed = 10.0f;
+
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f) * Time.deltaTime * movementSpeed;
+            transform.position = transform.position + movement;
+        }
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+}
